8d988b87ba14595a495f77c8712203c2c4e57eda
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-12 01:14:47 -0500

    Chore: removed unnecessary Dependencies

--- app/src/main/java/com/maheshgaya/android/readfromjson/CityAdapter.java
+++ app/src/main/java/com/maheshgaya/android/readfromjson/CityAdapter.java
@@ -2,8 +2,6 @@ package com.maheshgaya.android.readfromjson;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
